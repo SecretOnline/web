ccfb4aed0ea43994a8ca0372b673e0c945c7923d
secret_online <me+github@secretonline.co>
2016-11-10 13:06:01 +1300

    More linting fixes

--- src/components/DefaultHeadMeta/index.js
+++ src/components/DefaultHeadMeta/index.js
@@ -6,23 +6,22 @@ const DefaultHeadMeta = (props, { metadata: { pkg } }) => (
     <Helmet
       meta={ [
           {
-              name: 'generator', content: `${
-          process.env.PHENOMIC_NAME } ${ process.env.PHENOMIC_VERSION }`,
+              name: 'generator', content: `${process.env.PHENOMIC_NAME } ${ process.env.PHENOMIC_VERSION }`
           },
         { property: 'og:site_name', content: pkg.name },
-        { name: 'twitter:site', content: `@${ pkg.twitter }` },
+        { name: 'twitter:site', content: `@${ pkg.twitter }` }
       ] }
       script={ [
         { src: 'https://cdn.polyfill.io/v2/polyfill.min.js' },
-        {'type': 'application/javascript', 'innerHTML': '(function(l,o,a,d,e,r){d=o.head;function f(){a.forEach(function(u){r=o.createElement(\'link\');r.rel=\'stylesheet\';r.href=u;d.appendChild(r);});}e=requestAnimationFrame;if (e)e(f);else l.addEventListener(\'load\',f);}(window,document,[\'https://fonts.googleapis.com/css?family=Open+Sans\',\'https://fonts.googleapis.com/css?family=Roboto+Mono\']));'},
+        {'type': 'application/javascript', 'innerHTML': '(function(l,o,a,d,e,r){d=o.head;function f(){a.forEach(function(u){r=o.createElement(\'link\');r.rel=\'stylesheet\';r.href=u;d.appendChild(r);});}e=requestAnimationFrame;if (e)e(f);else l.addEventListener(\'load\',f);}(window,document,[\'https://fonts.googleapis.com/css?family=Open+Sans\',\'https://fonts.googleapis.com/css?family=Roboto+Mono\']));'}
       ] }
     />
 
     { /* meta viewport safari/chrome/edge */ }
     <Helmet
-      meta={ [ {
-          name: 'viewport', content: 'width=device-width, initial-scale=1',
-      } ] }
+      meta={[{
+          name: 'viewport', content: 'width=device-width, initial-scale=1'
+      }]}
     />
     <style>{ '@-ms-viewport { width: device-width; }' }</style>
   </div>
