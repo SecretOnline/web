f5eeb5e7fcf0289a9c504f869ca1ffcebcddda49
secret_online <me+github@secretonline.co>
2016-11-15 15:31:08 +1300

    Add markdown plugin
    For now, it's exactly the same as the default one. I've just moved it out of there, and into here. This way, I'll be able to add more addons for remark without having to edit phenomic itself.

---
+++ webpack/md-plugin.js
@@ -0,0 +1,40 @@
+const remark = require('remark');
+const slug = require('remark-slug');
+const autoLinkHeadings = require('remark-autolink-headings');
+const highlight = require('remark-highlight.js');
+const toc = require('remark-toc');
+const html = require('remark-html');
+
+const autolinkConf = {
+  content: {
+    type: 'text',
+    value: '#',
+  },
+  linkProperties: {
+    className: 'phenomic-HeadingAnchor',
+  }
+};
+const htmlConf = {
+  entities: 'escape'
+};
+const processConf = {
+  commonmark: true
+};
+
+function transformMd(text) {
+  return remark()
+    .use(slug)
+    .use(autoLinkHeadings, autolinkConf)
+    .use(html, htmlConf)
+    .use(highlight)
+    .use(toc)
+    .process(text, processConf)
+    .toString();
+}
+
+function mdPlugin({result}) {
+  return {
+    ...result,
+    body: transformMd(result.body)
+  };
+}
--- webpack/phenomic-plugins.js
+++ webpack/phenomic-plugins.js
@@ -3,12 +3,13 @@ const loaderHeadConf = require('phenomic/lib/loader-plugin-init-head-property-fr
 const loaderHeadContent = require('phenomic/lib/loader-plugin-init-head-property-from-content');
 const loaderBodyContent = require('phenomic/lib/loader-plugin-init-body-property-from-content');
 const loaderHeadDesc = require('phenomic/lib/loader-plugin-markdown-init-head.description-property-from-content');
-const loaderMd = require('phenomic/lib/loader-plugin-markdown-transform-body-property-to-html');
+
+const mdPlugin = require('./md-plugin');
 
 module.exports = [
   loaderHeadConf.default,
   loaderHeadContent.default,
   loaderBodyContent.default,
   loaderHeadDesc.default,
-  loaderMd.default
+  mdPlugin
 ];
