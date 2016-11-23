3bf9a2e5c46b81bf218696e2b79229548944a8ec
secret_online <me+github@secretonline.co>
2016-11-23 19:53:57 +1300

    Add edit date to Post grey matter

--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -7,22 +7,33 @@ import styles from './index.css';
 const Post = (props) => {
   let {head} = props;
 
-  // it's up to you to choose what to do with this layout ;)
   const pageDate = head.date ? new Date(head.date) : null;
+  const editDate = head.date ? new Date(head.edited) : null;
+
+
+  let dateNodes = [];
+  if (pageDate) {
+    dateNodes.push(
+      <em><time key={ pageDate.toISOString() }>
+        { pageDate.toDateString() }
+      </time></em>
+    );
+  }
+  if (pageDate && editDate) {
+    dateNodes.push(<br />);
+  }
+  if (editDate) {
+    dateNodes.push(
+      <em><time key={ editDate.toISOString() }>
+        Edited: { editDate.toDateString() }
+      </time></em>
+    );
+  }
 
   return (
     <Page
       { ...props }
-      header={
-        <div className={styles.time}>
-          {
-          pageDate &&
-          <em><time key={ pageDate.toISOString() }>
-            { pageDate.toDateString() }
-          </time></em>
-        }
-        </div>
-      }
+      header={<div className={styles.time}>{dateNodes}</div>}
     />
   );
 };
