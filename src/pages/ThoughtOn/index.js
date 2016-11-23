689a68325a6932313a80f375f620310b5195ed11
secret_online <me+github@secretonline.co>
2016-11-23 20:03:28 +1300

    Duplicate Post object

---
+++ src/pages/ThoughtOn/index.js
@@ -0,0 +1,45 @@
+import React, { PropTypes } from 'react';
+
+import Page from '../Page';
+
+import styles from './index.css';
+
+const ThoughtOn = (props) => {
+  let {head} = props;
+
+  const pageDate = head.date ? new Date(head.date) : null;
+  const editDate = head.date ? new Date(head.edited) : null;
+
+
+  let dateNodes = [];
+  if (pageDate) {
+    dateNodes.push(
+      <em><time key={ pageDate.toISOString() }>
+        { pageDate.toDateString() }
+      </time></em>
+    );
+  }
+  if (pageDate && editDate) {
+    dateNodes.push(<br />);
+  }
+  if (editDate) {
+    dateNodes.push(
+      <em><time key={ editDate.toISOString() }>
+        Edited: { editDate.toDateString() }
+      </time></em>
+    );
+  }
+
+  return (
+    <Page
+      { ...props }
+      header={<div className={styles.time}>{dateNodes}</div>}
+    />
+  );
+};
+
+ThoughtOn.propTypes = {
+  head: PropTypes.object.isRequired,
+};
+
+export default ThoughtOn;
