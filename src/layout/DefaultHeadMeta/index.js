2171df83c7d4499eaa90a3d3627906c246003607
secret_online <me+github@secretonline.co>
2016-11-29 13:23:07 +1300

    Add robots and humans.txt

--- src/layout/DefaultHeadMeta/index.js
+++ src/layout/DefaultHeadMeta/index.js
@@ -14,6 +14,9 @@ const DefaultHeadMeta = (props, { metadata: { pkg } }) => (
         {name: 'apple-mobile-web-app-capable', content: 'yes'},
         {name: 'mobile-web-app-capable', content: 'yes'}
       ] }
+      link={[
+        {rel: 'author', href: '/humans.txt'},
+      ]}
       script={ [
         {src: 'https://cdn.polyfill.io/v2/polyfill.min.js'},
         {type: 'application/javascript', innerHTML: '(function(l,o,a,d,e,r){d=o.head;function f(){a.forEach(function(u){r=o.createElement(\'link\');r.rel=\'stylesheet\';r.href=u;d.appendChild(r);});}e=requestAnimationFrame;if (e)e(f);else l.addEventListener(\'load\',f);}(window,document,[\'https://fonts.googleapis.com/css?family=Roboto+Mono\']));'}
