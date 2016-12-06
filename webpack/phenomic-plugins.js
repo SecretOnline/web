1990b26de5eced84771d6ea990e2829d4b841a86
secret_online <me+github@secretonline.co>
2016-12-07 10:17:56 +1300

    Wrap image tags with <figure>

--- webpack/phenomic-plugins.js
+++ webpack/phenomic-plugins.js
@@ -4,6 +4,7 @@ const loaderHeadContent = require('phenomic/lib/loader-plugin-init-head-property
 const loaderBodyContent = require('phenomic/lib/loader-plugin-init-body-property-from-content').default;
 const loaderHeadDesc = require('phenomic/lib/loader-plugin-markdown-init-head.description-property-from-content').default;
 
+const imgWrapper = require('./img-wrapper');
 const mdPlugin = require('./md-plugin');
 
 module.exports = [
@@ -11,5 +12,6 @@ module.exports = [
   loaderHeadContent,
   loaderBodyContent,
   loaderHeadDesc,
+  imgWrapper,
   mdPlugin
 ];
